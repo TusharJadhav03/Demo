4fb9c3fc0bcd0543d763601c0e6b751b8dde8c33
Tushar <tushar.jadhav.work@gmail.com>
2024-04-30 22:16:28 +0530

    Name Added Tushar

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,5 +1,9 @@
-public class Main {
+public class EmployeeWage {
+
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        System.out.println("Hello Tushar");
     }
 }
